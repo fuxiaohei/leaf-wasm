24247763a3a9ef264cd13073c756787e11ae4357
fuxiaohei <fuxiaohei@vip.qq.com>
2022-11-22 00:47:18 +0800

    feat(cli): better error handle when starting server

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,27 +1,32 @@
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Error, Request, Response, Server};
 use log::{error, info};
-use std::convert::Infallible;
 use std::net::SocketAddr;
 
-async fn handler(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+async fn handler(_req: Request<Body>) -> Result<Response<Body>, Error> {
     Ok(Response::new(Body::from("Hello, World")))
 }
 
 pub async fn start(addr: SocketAddr) {
     info!("Starting server on {}", addr);
-    
+
     // A `Service` is needed for every connection, so this
     // creates one from our `hello_world` function.
-    let make_svc = make_service_fn(|_conn| async {
+    let make_service = make_service_fn(|_conn| async {
         // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(handler))
+        Ok::<_, Error>(service_fn(handler))
     });
 
-    let server = Server::bind(&addr).serve(make_svc);
+    let server = match Server::try_bind(&addr) {
+        Ok(server) => server.serve(make_service),
+        Err(e) => {
+            error!("Starting server error, failed to bind: {}", e);
+            return;
+        }
+    };
 
     // Run this server for... forever!
     if let Err(e) = server.await {
-        error!("Start server error: {}", e);
+        error!("Starting server error: {}", e);
     }
 }
