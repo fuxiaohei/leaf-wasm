cf77f79408e3fd4d5a2a1cb47f087ca16c61e55d
fuxiaohei <fuxiaohei@vip.qq.com>
2022-12-02 21:54:04 +0800

    feat: add sdk impl

---
+++ crates/leaf-sdk-impl/src/lib.rs
@@ -0,0 +1,81 @@
+use std::str::FromStr;
+
+use bytes::Bytes;
+
+wit_bindgen_guest_rust::generate!("../../wit/leaf-http.wit");
+
+pub type Request = http::Request<Option<bytes::Bytes>>;
+pub type Response = http::Response<Option<bytes::Bytes>>;
+
+fn handle_sdk_http(req: Request) -> Response {
+    let url = req.uri();
+    let method = req.method().as_str().to_uppercase();
+
+    use http::header::HeaderName;
+
+    let mut builder = http::Response::builder().status(200);
+    let headers = builder.headers_mut().unwrap();
+    headers.insert(
+        HeaderName::from_static("X-Request-Method"),
+        method.parse().unwrap(),
+    );
+    headers.insert(
+        HeaderName::from_static("X-Request-Url"),
+        url.to_string().parse().unwrap(),
+    );
+
+    builder.body(Some(Bytes::from("Hello, World"))).unwrap()
+}
+
+impl TryFrom<leaf_http::Request> for http::Request<Option<bytes::Bytes>> {
+    type Error = anyhow::Error;
+
+    fn try_from(leaf_req: leaf_http::Request) -> Result<Self, Self::Error> {
+        let mut http_req = http::Request::builder()
+            .method(http::Method::from_str(leaf_req.method.as_str())?)
+            .uri(&leaf_req.uri);
+
+        for (key, value) in leaf_req.headers {
+            http_req = http_req.header(key, value);
+        }
+
+        let body = match leaf_req.body {
+            Some(b) => b.to_vec(),
+            None => Vec::new(),
+        };
+
+        let body = Some(bytes::Bytes::from(body));
+
+        Ok(http_req.body(body)?)
+    }
+}
+
+impl TryFrom<http::Response<Option<bytes::Bytes>>> for leaf_http::Response {
+    type Error = anyhow::Error;
+
+    fn try_from(http_res: http::Response<Option<bytes::Bytes>>) -> Result<Self, Self::Error> {
+        let status = http_res.status().as_u16();
+        let mut headers: Vec<(String, String)> = vec![];
+        for (key, value) in http_res.headers() {
+            headers.push((key.to_string(), value.to_str()?.to_string()));
+        }
+        let body = http_res.body().as_ref().map(|b| b.to_vec());
+        Ok(leaf_http::Response {
+            status,
+            headers,
+            body,
+        })
+    }
+}
+
+struct HttpImpl;
+
+impl leaf_http::LeafHttp for HttpImpl {
+    fn handle_request(req: leaf_http::Request) -> leaf_http::Response {
+        let http_req: Request = req.try_into().unwrap();
+        let http_resp = handle_sdk_http(http_req);
+        http_resp.try_into().unwrap()
+    }
+}
+
+export_leaf_http!(HttpImpl);
--- tests/integration.rs
+++ tests/integration.rs
@@ -1,8 +1,8 @@
 use std::process::Command;
 
-/// rust sample test
+/// rust leaf-abi-impl test
 #[test]
-fn run_wasm_component_exports_test() {
+fn run_leaf_abi_exports_test() {
     // run command 'cargo build -p "leaf-abi-impl" --release --target wasm32-unknown-unknown'
     let result = Command::new("cargo")
         .arg("build")
@@ -16,6 +16,37 @@ fn run_wasm_component_exports_test() {
     println!("status: {}", result.status);
 
     let target_file = "target/wasm32-unknown-unknown/release/leaf_abi_impl.wasm";
+    // check target file exist
+    assert!(std::path::Path::new(target_file).exists());
+
+    // encode wasm module to wasm component
+    let component_file = match component_compiler::encode(target_file, None) {
+        Ok(file) => file,
+        Err(err) => panic!("encode component failed: {}", err),
+    };
+    // check component generated
+    assert!(std::path::Path::new(&component_file).exists());
+
+    // check http handler is exported in wasm component
+    assert!(component_compiler::check_http_handler(component_file.to_str().unwrap()).is_ok());
+}
+
+/// run leaf-sdk-impl test
+#[test]
+fn run_leaf_sdk_exports_test() {
+    // run command 'cargo build -p "leaf-sdk-impl" --release --target wasm32-unknown-unknown'
+    let result = Command::new("cargo")
+        .arg("build")
+        .arg("-p")
+        .arg("leaf-sdk-impl")
+        .arg("--release")
+        .arg("--target")
+        .arg("wasm32-unknown-unknown")
+        .output()
+        .expect("failed to build leaf-sdk-impl");
+    println!("status: {}", result.status);
+
+    let target_file = "target/wasm32-unknown-unknown/release/leaf_sdk_impl.wasm";
     // check target file exist
     assert!(std::path::Path::new(target_file).exists());
 
