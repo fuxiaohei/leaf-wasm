f122b5f5988af639a66f1beef3c82abc28306ff6
fuxiaohei <fuxiaohei@vip.qq.com>
2023-01-05 16:02:54 +0800

    feat(cli): add wasm flag to up command to run wasm file

--- src/cmd_cli/up.rs
+++ src/cmd_cli/up.rs
@@ -6,35 +6,46 @@ use std::net::SocketAddr;
 
 #[derive(Args, Debug)]
 pub struct UpCommand {
-    // The port to listen on
+    /// The port to listen on
     #[clap(long, default_value("0.0.0.0:18899"))]
     pub addr: Option<SocketAddr>,
+    /// The wasm file to run, ignore current project
+    #[clap(long)]
+    pub wasm: Option<String>,
 }
 
 impl UpCommand {
     pub async fn run(&self) {
-        // load manifest file
-        let manifest_file = DEFAULT_MANIFEST_FILE;
-        let manifest = match Manifest::from_file(manifest_file) {
-            Ok(manifest) => manifest,
-            Err(e) => {
-                error!("read manifest file error: {}", e);
-                return;
-            }
-        };
-        info!("[Main] read manifest '{:?}'", manifest_file);
+        let wasm_file = if self.wasm.is_some() {
+            info!(
+                "[Main] use wasm file from command line: {:?}",
+                self.wasm.as_ref().unwrap()
+            );
+            self.wasm.clone().unwrap()
+        } else {
+            // load manifest file
+            let manifest_file = DEFAULT_MANIFEST_FILE;
+            let manifest = match Manifest::from_file(manifest_file) {
+                Ok(manifest) => manifest,
+                Err(e) => {
+                    error!("read manifest file error: {}", e);
+                    return;
+                }
+            };
+            info!("[Main] read manifest '{:?}'", manifest_file);
 
-        let wasm_file = match manifest.determine_target() {
-            Ok(file) => file,
-            Err(e) => {
-                error!("[Main] find wasm error: {}", e);
-                return;
+            match manifest.determine_target() {
+                Ok(file) => file,
+                Err(e) => {
+                    error!("[Main] find wasm error: {}", e);
+                    return;
+                }
             }
         };
 
         if !std::path::PathBuf::from(&wasm_file).exists() {
             error!("[Worker] file not found: {}", &wasm_file);
-            info!("[Worker] Try to run 'leaf-cli compile' to compile wasm file");
+            info!("[Worker] Try to run 'leaf-cli compile' to build wasm file");
             return;
         }
         info!("[Worker] use file: {}", &wasm_file);
