ccd36fd4b226a86fe889b8ac022b6306c003465c
fuxiaohei <fuxiaohei@vip.qq.com>
2023-01-15 22:53:01 +0800

    test: add local server test

--- src/local_server/mod.rs
+++ src/local_server/mod.rs
@@ -28,3 +28,33 @@ pub async fn start(addr: SocketAddr, wasm: String, enable_wasi: bool) {
         error!("[Server] starting error: {}", e);
     }
 }
+
+#[tokio::test]
+async fn run_local_server_test() {
+    let handle = tokio::spawn(async {
+        start(
+            "127.0.0.1:18899".parse().expect("wrong socket addr"),
+            "./tests/sample.wasm".to_string(),
+            false,
+        )
+        .await;
+    });
+
+    // wait for server to start
+    tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+
+    let resp = reqwest::get("http://127.0.0.1:18899/abc")
+        .await
+        .expect("failed to get /abc");
+    println!("{:?}", resp);
+
+    assert!(resp.status().is_success());
+    let headers = resp.headers();
+    assert_eq!(headers.get("content-length").unwrap(), "12");
+    assert_eq!(headers.get("x-request-url").unwrap(), "/abc");
+    assert_eq!(headers.get("x-request-method").unwrap(), "GET");
+    assert!(resp.text().await.unwrap().contains("Hello, World"));
+
+    handle.abort();
+    assert!(handle.await.unwrap_err().is_cancelled());
+}
