8f48f29b17264cd9a08f51fcb6ad339c0cf1f72f
fuxiaohei <fuxiaohei@vip.qq.com>
2023-02-06 22:31:04 +0800

    feat(kv): add basic kv host-impl

---
+++ crates/host-impl/src/kv.rs
@@ -0,0 +1,45 @@
+wasmtime::component::bindgen!({
+    world:"kv",
+    path: "../../wit/kv.wit",
+    async: true,
+});
+
+use async_trait::async_trait;
+use kv_imports::OpResult;
+
+/// KvImpl is the implementation of the `kv` component.
+pub struct KvImpl {
+    pub user_uuid: String,
+    pub namespace: String,
+}
+
+impl KvImpl {
+    /// new creates a new `KvImpl` instance.
+    pub fn new(user_uuid: String, namespace: String) -> Self {
+        KvImpl {
+            user_uuid,
+            namespace,
+        }
+    }
+}
+
+#[async_trait]
+impl kv_imports::KvImports for KvImpl {
+    /// set sets a key-value pair in the key-value store.
+    async fn set(&mut self, key: String, value: String, ttl: u32) -> anyhow::Result<OpResult> {
+        Ok(OpResult::Success)
+    }
+
+    /// get gets a value from the key-value store.
+    async fn get(&mut self, key: String) -> anyhow::Result<Result<String, OpResult>> {
+        Ok(Ok("value".to_string()))
+    }
+
+    /// list_values lists all key-value pairs in the key-value store.
+    async fn list_values(&mut self) -> anyhow::Result<Result<Vec<String>, OpResult>> {
+        Ok(Ok(vec!["key1,value1".to_string()]))
+    }
+}
+
+/// export the `kv` component.
+pub use kv_imports::add_to_linker;
--- crates/worker/src/context.rs
+++ crates/worker/src/context.rs
@@ -1,10 +1,11 @@
-use leaf_host_impl::http::FetchImpl;
+use leaf_host_impl::{http::FetchImpl, kv::KvImpl};
 use wasi_host::WasiCtx;
 use wasi_host_cap_std_sync::WasiCtxBuilder;
 
 pub struct Context {
     pub wasi: WasiCtx,
     pub fetch: FetchImpl,
+    pub kv: KvImpl,
 }
 
 impl Context {
@@ -14,11 +15,15 @@ impl Context {
     pub fn fetch(&mut self) -> &mut FetchImpl {
         &mut self.fetch
     }
+    pub fn kv(&mut self) -> &mut KvImpl {
+        &mut self.kv
+    }
     pub fn new(req_id: u64) -> Self {
         let wasi = WasiCtxBuilder::new().inherit_stdio().build();
         Self {
             wasi,
             fetch: FetchImpl::new(req_id),
+            kv: KvImpl::new("uuid".to_string(), "ns".to_string()),
         }
     }
 }
