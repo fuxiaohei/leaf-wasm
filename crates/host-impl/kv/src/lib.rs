0c06e185fec99d2e6579bc018d4f77e3b42ac3e7
fuxiaohei <fuxiaohei@vip.qq.com>
2023-02-07 23:33:43 +0800

    feat(kv): add memory kv provider

---
+++ crates/host-impl/kv/src/lib.rs
@@ -0,0 +1,23 @@
+/// KvError is the error type for the kv store.
+#[derive(Debug)]
+pub enum Error {
+    /// OperationFailed is returned when the operation failed.
+    OperationFailed,
+    /// Expired is returned when the key has expired.
+    Expired,
+    /// NotExist is returned when the key was not found.
+    NotExist,
+    /// Ok is returned when the operation was successful.
+    Ok,
+}
+
+/// Provider is the interface for a key-value store.
+#[async_trait::async_trait]
+pub trait Provider {
+    async fn get(&mut self, key: String) -> Result<String, Error>;
+    async fn set(&mut self, key: String, value: String, ttl: u32) -> Result<(), Error>;
+    async fn delete(&mut self, key: String) -> Result<(), Error>;
+    async fn list(&mut self, prefix: String) -> Result<Vec<(String, String)>, Error>;
+}
+
+mod memory;
